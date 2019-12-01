18b6faaff7f80c634cb61da1a48d84a47d99cab8
Onni Kosomaa <onni.kosomaa@hotmail.com>
2019-12-01 23:35:02 +0200

    control_mc: Add slave SPI protocol

--- control_mc/constants.h
+++ control_mc/constants.h
@@ -3,18 +3,15 @@
 
 #include <avr/io.h>
 
-// Clock rate: 1 / 128
-#define SPI_SLAVE_CLOCK_RATE ((1 << SPR0) | (1 << SPR1))
-
 // DAC defines
 // Clock rate: 1 / 128
 #define DAC_CLOCK_RATE      ((1 << SPR0) | (1 << SPR1))
 
 // Data direction registers
-#define DD_DAC_CS           DDRE
-#define DD_DAC_CS_PIN       DDE2
-#define DD_DAC_LDAC         DDRC
-#define DD_DAC_LDAC_PIN     DDC2
+#define DDR_DAC_CS          DDRE
+#define DD_DAC_CS           DDE2
+#define DDR_DAC_LDAC        DDRC
+#define DD_DAC_LDAC         DDC2
 
 // Port registers (output high/low selection)
 #define PORT_DAC_CS         PORTE
@@ -26,18 +23,26 @@
 // Just a plain divider
 #define TWI_CLOCK_DIV 0xFF
 
-// Pins
-#define DDR_MISO0 DDRB
-#define DD_MISO0  DDB4
+// SPI pins
+#define DDR_MISO0       DDRB
+#define DD_MISO0        DDB4
 
-#define DDR_MOSI1 DDRE
-#define DD_MOSI1  DDE3
-#define DDR_SCK1  DDRC
-#define DD_SCK1   DDC1
+#define PIN_SS0         PINB
+#define PIN_SS0_PIN     PINB2
 
-#define DDR_SCL0  DDRC
-#define DD_SCL0   DDC5
-#define DDR_SDA0  DDRC
-#define DD_SDA0   DDC4
+#define PORT_SPI0       PORTB
+#define PORT_MOSI0_PIN  PORTB3
+#define PORT_SCK0_PIN   PORTB5
+#define PORT_SS0_PIN    PORTB2
+
+#define DDR_MOSI1       DDRE
+#define DD_MOSI1        DDE3
+#define DDR_SCK1        DDRC
+#define DD_SCK1         DDC1
+
+#define DDR_SCL0        DDRC
+#define DD_SCL0         DDC5
+#define DDR_SDA0        DDRC
+#define DD_SDA0         DDC4
 
 #endif
--- control_mc/main.c
+++ control_mc/main.c
@@ -1,27 +1,72 @@
 #include "constants.h"
+#include "spi_protocol.h"
 
 #include <avr/cpufunc.h>
 #include <avr/interrupt.h>
 #include <avr/wdt.h>
+#include <util/delay.h>
 
 #include <stdbool.h>
+#include <string.h>
 
+#define SPI_BUFFER_SIZE 5
+
+uint8_t spi_rx_buffer[SPI_BUFFER_SIZE] = { 0 };
+uint8_t spi_tx_buffer[SPI_BUFFER_SIZE] = { 0 };
+uint8_t spi_buf_length = 0;
+bool spi_packet_ready = false;
+
+struct {
+    uint32_t voltage;
+    uint32_t current;
+} typedef OutputValues;
 
 // Always disable interrupts when accessing.
-static uint32_t set_voltage  = 0U;
-static uint32_t read_voltage = 0U;
+static OutputValues set_values;
+static OutputValues read_values;
 
-static uint32_t set_current  = 0U;
-static uint32_t read_current = 0U;
+ISR(SPI0_STC_vect)
+{
+    if (spi_buf_length < SPI_BUFFER_SIZE)
+    {
+        spi_rx_buffer[spi_buf_length] = SPDR0;
+        SPDR0 = spi_tx_buffer[spi_buf_length];
+        spi_buf_length++;
+    }
+}
 
+// Use an interrupt on the slave SPI chip select pin
+// to reset the SPI rx/tx buffers.
+ISR(PCINT1_vect)
+{
+    // Falling edge means start of SPI transfer
+    if ((PIN_SS0 & (1 << PIN_SS0_PIN)) == 0) {
+        spi_packet_ready = false;
+        spi_buf_length = 0;
+    } else {
+        spi_packet_ready = true;
+    }
+}
 
 static void setupSlaveSPI(void)
 {
     // Set data direction
     DDR_MISO0 |= 1 << DD_MISO0;
 
+    // Enable pull-ups as the optocouplers are open-collector
+    PORT_SPI0 |= (1 << PORT_MOSI0_PIN) |
+                 (1 << PORT_SCK0_PIN)  |
+                 (1 << PORT_SS0_PIN);
+
+    // Enable pin-change interrupt on chip select pin
+    PCICR |= (1 << PCIE1); // Enable pin change interrupt 1
+    PCMSK1 |= (1 << PCINT14); // Enable interrupt for pin 14 (chip select)
+
     // Enable SPI, with interrupt
-    SPCR0 = (1 << SPE) | (1 << SPIE) | SPI_SLAVE_CLOCK_RATE;
+    SPCR0 = (1 << SPE) |
+            (1 << SPIE) |
+            (MAIN_SPI_CPOL << CPOL) |
+            (MAIN_SPI_CPHA << CPHA);
 }
 
 static void setupDAC(void)
@@ -29,13 +74,13 @@ static void setupDAC(void)
     // Set data directions
     DDR_SCK1    |= 1 << DD_SCK1;
     DDR_MOSI1   |= 1 << DD_MOSI1;
-    DD_DAC_CS   |= 1 << DD_DAC_CS_PIN;
-    DD_DAC_LDAC |= 1 << DD_DAC_LDAC_PIN;
+    DDR_DAC_CS   |= 1 << DD_DAC_CS;
+    DDR_DAC_LDAC |= 1 << DD_DAC_LDAC;
 
     // Enable SPI, master mode, no interrupt
     SPCR1 = (1 << SPE1) | (1 << MSTR1) | DAC_CLOCK_RATE;
     // Lift chip select
-    PORT_DAC_CS |= 1 << PORT_DAC_CS;
+    PORT_DAC_CS |= 1 << PORT_DAC_CS_PIN;
 }
 
 static void setupADC(void)
@@ -50,33 +95,33 @@ static void setupADC(void)
 // Writes the set values for current and voltage to the DAC.
 // Blocks until complete.
 // Data protocol: http://ww1.microchip.com/downloads/en/DeviceDoc/20002249B.pdf
-static void writeDAC(void)
+static void writeDAC(OutputValues *values)
 {
+    // Make a local copy to avoid data races
     cli();
-    uint32_t curr = set_current;
-    uint32_t volt = set_voltage;
+    OutputValues tmp_values = *values;
     sei();
 
     uint8_t bytes[4];
 
-    uint8_t enable_volt = (volt > 0) ? 1 : 0;
-    uint8_t enable_curr = (curr > 0) ? 1 : 0;
+    uint8_t enable_volt = (tmp_values.voltage > 0) ? 1 : 0;
+    uint8_t enable_curr = (tmp_values.current > 0) ? 1 : 0;
 
-    bytes[0] = (1 << 7) |            // Channel A
-               (1 << 5) |            // Gain 1x
-               (enable_volt << 4) |  // Enable ouput
-               ((volt >> 8) & 0x0F); // 4 MSBs.
-    bytes[1] = volt & 0xFF;
+    bytes[0] = (1 << 7) |           // Channel A
+               (1 << 5) |           // Gain 1x
+               (enable_curr << 4) | // Enable ouput
+               ((tmp_values.current >> 8) & 0x0F); // 4 MSBs.
+    bytes[1] = tmp_values.current & 0xFF;
 
-    bytes[2] = (0 << 7) |            // Channel B
-               (1 << 5) |            // Gain 1x
-               (enable_curr << 4) |  // Enable ouput
-               ((curr >> 8) & 0x0F); // 4 MSBs.
-    bytes[3] = curr & 0xFF;
+    bytes[2] = (0 << 7) |           // Channel B
+               (1 << 5) |           // Gain 1x
+               (enable_volt << 4) | // Enable ouput
+               ((tmp_values.voltage >> 8) & 0x0F); // 4 MSBs.
+    bytes[3] = tmp_values.voltage & 0xFF;
 
 
     // Write channel A
-    PORT_DAC_CS &= ~(1 << PORT_DAC_CS);
+    PORT_DAC_CS &= ~(1 << PORT_DAC_CS_PIN);
     {
         SPDR1 = bytes[0];
         // Wait until the byte is transferred.
@@ -84,19 +129,19 @@ static void writeDAC(void)
         SPDR1 = bytes[1];
         while (!(SPSR1 & (1 << SPIF1))) { };
     }
-    PORT_DAC_CS |= 1 << PORT_DAC_CS;
+    PORT_DAC_CS |= 1 << PORT_DAC_CS_PIN;
 
     // 1 CPU cycle is enough for minimum CS high time
 
     // Write channel B
-    PORT_DAC_CS &= ~(1 << PORT_DAC_CS);
+    PORT_DAC_CS &= ~(1 << PORT_DAC_CS_PIN);
     {
         SPDR1 = bytes[2];
         while (!(SPSR1 & (1 << SPIF1))) { };
         SPDR1 = bytes[3];
         while (!(SPSR1 & (1 << SPIF1))) { };
     }
-    PORT_DAC_CS |= 1 << PORT_DAC_CS;
+    PORT_DAC_CS |= 1 << PORT_DAC_CS_PIN;
 
     // Latch outputs, with some delay to match minimum timing
     _NOP();
@@ -111,17 +156,45 @@ int main(void)
     // https://www.nongnu.org/avr-libc/user-manual/group__avr__watchdog.html
     wdt_enable(WDTO_1S);
 
-    setupSlaveSPI();
     setupDAC();
     setupADC();
 
     // Enable global interrupts
     sei();
 
+    // Initialize DAC output to zero
+    memset(&set_values, 0, sizeof(set_values));
+    memset(&read_values, 0, sizeof(read_values));
+    writeDAC(&set_values);
+
+    // Wait for SPI master to initiate communications
+    setupSlaveSPI();
     while (true) {
         wdt_reset();
+        if (spi_packet_ready) {
 
-        writeDAC();
+            uint8_t packet_type = 0;
+            cli();
+            {
+                // Make sure the packet has not been cleared before
+                // the cli instruction got executed.
+                if (spi_packet_ready) {
+                    spi_packet_ready = false;
+                    packet_type = spi_rx_buffer[0];
+                    if (packet_type == READWRITE && spi_buf_length == 5) {
+                        set_values.voltage = ((spi_rx_buffer[1] & 0x0F) << 8) |
+                                               spi_rx_buffer[2];
+                        set_values.current = ((spi_rx_buffer[3] & 0x0F) << 8) |
+                                               spi_rx_buffer[4];
+                    }
+                }
+            }
+            sei();
+
+            // Enable interrupts before actually writing the values to DAC
+            if (packet_type == READWRITE)
+                writeDAC(&set_values);
+        }
     }
 
     return 0;
